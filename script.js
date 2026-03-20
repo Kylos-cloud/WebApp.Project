858807960e00df381aeb5599bdc08f4ff24716a4
Kylos <ss4783309@gmail.com>
2026-03-20 16:59:27 +0800

    kylos1

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ leftBtn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+function openMenu() {
+  document.getElementById("categoryMenu").classList.add("active");
+  document.getElementById("overlay").classList.add("active");
+}
+
+function closeMenu() {
+  document.getElementById("categoryMenu").classList.remove("active");
+  document.getElementById("overlay").classList.remove("active");
+}
+
+const searchInput = document.getElementById("categorySearch");
+
+searchInput.addEventListener("input", function () {
+  const value = this.value.toLowerCase();
+  const items = document.querySelectorAll(".menu-item");
+
+  items.forEach(item => {
+    const text = item.textContent.toLowerCase();
+
+    if (text.includes(value)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
