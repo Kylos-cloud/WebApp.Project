a108b9639055f4479f4f132a42cecba894b7d40e
Kylos <ss4783309@gmail.com>
2026-05-24 01:13:46 +0800

    Wire frontend to deployed API fixes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: ['https://kylos-cloud.github.io', 'http://localhost:8080'] }));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
